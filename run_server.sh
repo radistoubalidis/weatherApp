c24496d37c9d561794b6b9dbda14729bb48658fd
radis toubalidis <rtoumpalidis@gmail.com>
2022-10-05 16:55:52 +0300

    test cron workflow(2)

--- run_server.sh
+++ run_server.sh
@@ -2,9 +2,9 @@ pip install flask pandas psycopg2 requests flask_cors
 docker-compose up -d 
 docker cp modules/schema.sql weatherapp_database_1:/
 docker exec -it weatherapp_database_1 psql -U postgres -f schema.sql
-export FLASK_APP=server.py
-export FLASK_ENV=development
-export FLASK_DEBUG=True
-apt-get install nohup
-nohup flask run > log.txt  2>&1 &
+sudo export FLASK_APP=server.py
+sudo export FLASK_ENV=development
+sudo export FLASK_DEBUG=True
+sudo apt-get install nohup
+sudo nohup flask run > log.txt  2>&1 &
 python cron.py
